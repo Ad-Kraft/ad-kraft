b3bceae668950c436e47513efa99c62071f11de9
Stefan Dragicevic <stefandragicevic@Stefan-Dragicevic.local>
2021-08-24 14:37:06 +0200

    Add services list

--- pages/usluge.vue
+++ pages/usluge.vue
@@ -22,7 +22,7 @@
                     <div class="col-md-6 mt-30" data-aos="fade-up" data-aos-delay="500">
                         <div class="service-card">
                             <div class="service-content">
-                                <h4 class="title">Google Marketing</h4>
+                                <h4 class="title">Digitalni marketing</h4>
                                 <p>Sve naše kampanje uvijek se završavaju na isti način - USPJEŠNO, to postižemo kroz individualnost i fokusiranost na kupca. Garantujemo postizanje postavljenih ciljeva, poput povećane prodaje ili veće informisanosti.</p>
                                 <n-link to="/service-details" class="btn btn-outline-danger">Details +</n-link>
                             </div>
@@ -57,7 +57,7 @@
                     <div class="col-md-4 mt-30" data-aos="fade-up" data-aos-delay="2000">
                         <div class="service-card">
                             <div class="service-content">
-                                <h4 class="title">SEO optimizacija</h4>
+                                <h4 class="title">Grafički dizajn</h4>
                                 <p>SEO nudi vidljivu i efikasnu prisutnost na internet pretraživačima što dovodi do značajnog povećanja prodaje, profitabilnosti i ekonomičnosti naših klijenata.</p>
                                 <n-link to="/service-details" class="btn btn-outline-secondary">Details +</n-link>
                             </div>
@@ -68,7 +68,7 @@
                     <div class="col-md-4 mt-30" data-aos="fade-up" data-aos-delay="2000">
                         <div class="service-card">
                             <div class="service-content">
-                                <h4 class="title">SEO optimizacija</h4>
+                                <h4 class="title">Društvene mreže</h4>
                                 <p>SEO nudi vidljivu i efikasnu prisutnost na internet pretraživačima što dovodi do značajnog povećanja prodaje, profitabilnosti i ekonomičnosti naših klijenata.</p>
                                 <n-link to="/service-details" class="btn btn-outline-secondary">Details +</n-link>
                             </div>
@@ -82,7 +82,7 @@
                     <div class="col-md-4 mt-30" data-aos="fade-up" data-aos-delay="1500">
                         <div class="service-card">
                             <div class="service-content">
-                                <h4 class="title">Društvene mreže</h4>
+                                <h4 class="title">SEO Optimizacija</h4>
                                 <p>Nikad ne znate koju društvenu mrežu Vaš klijent želi koristiti za interakciju s Vama. Naš tim stručnjaka iz oblasti društvenih medija brinuće za Vaše postojeće korisnike, istovremeno privlačeći nove.</p>
                                 <n-link to="/service-details" class="btn btn-outline-primary">Details +</n-link>
                             </div>
@@ -93,7 +93,7 @@
                     <div class="col-md-4 mt-30" data-aos="fade-up" data-aos-delay="2000">
                         <div class="service-card">
                             <div class="service-content">
-                                <h4 class="title">SEO optimizacija</h4>
+                                <h4 class="title">Optimizacija konverzija</h4>
                                 <p>SEO nudi vidljivu i efikasnu prisutnost na internet pretraživačima što dovodi do značajnog povećanja prodaje, profitabilnosti i ekonomičnosti naših klijenata.</p>
                                 <n-link to="/service-details" class="btn btn-outline-secondary">Details +</n-link>
                             </div>
@@ -104,7 +104,7 @@
                     <div class="col-md-4 mt-30" data-aos="fade-up" data-aos-delay="2000">
                         <div class="service-card">
                             <div class="service-content">
-                                <h4 class="title">SEO optimizacija</h4>
+                                <h4 class="title">Hosting</h4>
                                 <p>SEO nudi vidljivu i efikasnu prisutnost na internet pretraživačima što dovodi do značajnog povećanja prodaje, profitabilnosti i ekonomičnosti naših klijenata.</p>
                                 <n-link to="/service-details" class="btn btn-outline-secondary">Details +</n-link>
                             </div>

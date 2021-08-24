43629906a86a29cf9627a7b5e4b6ea9c54d626cf
Stefan Dragicevic <stefan.dragicevic0@gmail.com>
2021-08-24 13:56:51 +0200

    Revert "Add content for web design page"

--- pages/izrada-web-stranica.vue
+++ pages/izrada-web-stranica.vue
@@ -7,160 +7,9 @@
 
         <Breadcrumb title="Izrada web stranica" active-title="Izrada web stranica" />
 
-         <section class="service-details-section">
-            <div class="container">
-                <div class="row mtn-35">
-                    <div class="col-12 mt-35">
-                        <div class="service-details">
-                            <div class="service-details-list">
-                                <h2 class="title">Kupci Vam neće pružiti drugu priliku da napravite prvi utisak</h2>
-                                <h5 class="subtitle">U digitalnom svijetu prvi utisak je od vitalnog značaja, a izvanredan web dizajn je ključ uspjeha. Korisnici uživaju u posjećivanju web stranica koje su estetski privlačne i jednostavne za navigaciju. Naš tim će Vam pomoći da Vaša web stranica napravi izvanredan prvi utisak svojim dizajnom, brzinom i pristupačnošću.</h5>
+        <ServiceCarousel />
 
-                                <p>S našom agencijom Vaš posao će biti primijećen u digitalnom svijetu jer korištenjem genijalnih dizajnerskih rješenja Vašem brendu osiguravamo prepoznatljivost i uticaj. Nudimo različita prilagodljiva dizajnerska rješenja s fokusom na kreiranje web stranice koja će odgovarati Vašem brendu ujedno privlačeći veliki broj posjetilaca. Naš tim sarađuje s Vama dok ne napravimo savršen dizajn za Vaše potrebe.</p>
-
-                                <p>Vjerujemo da je uska saradnja temelj za izvanredne rezultate. Zbog toga sarađujemo na svakom koraku procesa koji vodi do izvrsnog poslovanja – od procjenjivanja Vaših potreba i želja do konkurentne analize.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="service-details">
-                    <div class="service-details-list">
-                        <div class="row mtn-35">
-                            <div class="col-lg-7 mt-35">
-                                <p>Za kreativan i funkcionalan web dizajn neophodna je detaljna analiza i sveobuhvatno istraživanje. Zbog toga naš tim na svakom koraku izrade web stranice sarađuje s Vama, osiguravajući da Vaša nova web stranica zaista predstavlja Vas i Vaše poslovanje istovremeno ostvarujući najvažnije ciljeve – kvalitetno korisničko iskustvo i konverziju.</p>
-
-                                <p>Naš rad započinje kreativnim i atraktivnim dizajnom koji najbolje predstavlja Vaš brend. Zatim proučavamo Vaše klijente kako bismo stvorili jedinstvene elemente i stil koji najbolje odgovara Vašem poslovanju ujedno privlačeći i zadržavajući Vaše klijente.</p>
-                            </div>
-                            <div class="col-lg-5 mt-35">
-                                <img src="/images/service/web/ad-kraft-klinika-kostic-web-sajt-web-design.jpg" alt="images-not_found" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="service-details">
-                    <div class="service-details-list">
-                        <div class="row">
-                            <div class="col-12 mt-5">
-                                <h2 class="title">Web programiranje</h2>
-                                <h5 class="subtitle">Imamo iskustva u svim aspektima web programiranja kako bismo pomogli našim klijentima da ostvare pun potencijal svog poslovanja. Osmišljavamo planove koji dovode do povećanja prodaje i privlačenja velikog broja potencijalnih klijenata.</h5>
-                                <p>Koristimo sveobuhvatan pristup web programiranju – od kodiranja do web dizajna i sadržaja. Naš cilj je razviti Vašu web stranicu ne samo kako bi se bismo poboljšali njenu vidljivosti u pretraživačima, već i kako bismo istinski pružili najbolje iskustvo Vašim korisnicima. Naš tim iskusnih profesionalaca garantuje Vam 100% zadovoljstvo.</p>
-
-                                <p>Nudimo širok spektar usluga koje uključuju e-commerce stranice, web stranice, portale, ekstranet, intranet, prilagođena softverska rješenja, izvorne i hibridne aplikacije za mobilne i tablet uređaje, Facebook aplikacije ili jednostavno web stranicu koja djeluje kao portfolio za prikazivanje Vaših usluga.</p>
-                            </div>
-
-                            
-                                <div class="col-lg-4">
-                                    <h5 class="subtitle">E-commerce rješenja</h5>
-                                    <p>Naša digitalna agencija nudi softver rješenja za e-commerce i internet prodavnice.</p>
-                                </div>
-                                <div class="col-lg-4">
-                                    <h5 class="subtitle">CMS, WordPress, Drupal</h5>
-                                    <p>Razvijamo sisteme za upravljanje sadržajem (CMS) kako bismo Vam omogućili samostalno održavanje i ažuriranje Vaše web stranice.</p>
-                                </div>
-                                <div class="col-lg-4">
-                                    <h5 class="subtitle">Intranet/Extranet</h5>
-                                    <p>Naš tim razvija prilagođena Intranet i Extranet rješenja kao i integraciju Sharepoint-a.</p>
-                                </div>
-                                <div class="col-lg-4">
-                                    <h5 class="subtitle">Facebook aplikacije</h5>
-                                    <p>Vodeća smo agencija za razvoj aplikacija koja nudi usluge razvoja web, mobilnih i Facebook aplikacija.</p>
-                                </div>
-                                <div class="col-lg-4">
-                                    <h5 class="subtitle">Email Marketing rješnja</h5>
-                                    <p>Kreiramo e-mail marketing softver koji će Vam olakšati izradu, slanje i praćenje e-mail kampanja koje donose rezultate.</p>
-                                </div>
-                                <div class="col-lg-4">
-                                    <h5 class="subtitle">PHP i JS</h5>
-                                    <p>Pružamo usluge razvoja softvera u: PHP, Drupal, Python, JavaScript / jQuery, Vue.js i drugim modernim tehnologijama.</p>
-                                </div>
-                            
-                        </div>
-                    </div>
-                </div>
-
-                <div class="service-details">
-                    <div class="service-details-list">
-                        <div class="row mt-35">
-                            <div class="col-lg-6 mt-35">
-                                <img class="experience" src="/images/service/web/ad-kraft-web-design-testiranje-i-provjera-kvaliteta.png" width="300px" alt="images-not_found" />
-                            </div>
-
-                            <div class="col-12 col-lg-6 mt-35">
-                                <h2 class="title">Testiranje i provjera kvaliteta</h2>
-                                <p>Provodimo rigorozna testiranja na svim uređajima, internet pretraživačima i operativnim sistemima prije nego što pokrenemo Vašu web stranicu. Testiranja se vrše sa aspekta korisnika kao i administratora kako bi se osiguralo da je Vaša web stranica funkcionalna, responzivna i ispunjava sve Vaše potrebe.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="service-details">
-                    <div class="service-details-list">
-                        <div class="row mt-35">
-                            <div class="col-12 col-lg-6 mt-35">
-                                <h2 class="title">Sigurnost</h2>
-                                <p>Koristimo alate i sigurno okruženje za testiranje kako bismo obezbijedili da Vaša aplikacija bude besprijekorna prije nego što dođe do krajnjih korisnika. Tokom procesa testiranja Vi imate kompletan uvidu u procese i pristup istim.</p>
-                            </div>
-
-                            <div class="col-lg-6 mt-35">
-                                <img class="experience" src="/images/service/web/ad-kraft-web-design-sigurnost.png" width="300px" alt="images-not_found" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- <div class="service-details">
-                    <div class="service-details-list">
-                        <div class="row">
-                            <div class="col-12">
-                                <h2 class="title">SEO Strategy</h2>
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknowne printer took a galley of type andscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknowne printer took a galley of type andscrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent ially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div> -->
-                <div class="row g-0">
-                    <div class="col-12">
-                        <div class="next-prev-section">
-                            <div class="row align-items-center align-items-xl-start">
-                                <div class="col-sm-4 col-6">
-                                    <div class="service-prev service-media">
-                                        <div class="img">
-                                            <img src="/images/service-details/sm-1.png" alt="images-not_found" />
-                                        </div>
-                                        <div class="content">
-                                            <h4 class="title">Digital Marketing</h4>
-                                            <n-link to="/service-details" class="link">Previous <i class="icofont-rounded-double-right"></i></n-link>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-sm-4 d-none d-sm-block">
-                                    <div class="service-dots text-center">
-                                        <img src="/images/service-details/dots.png" alt="images-not_found" />
-                                    </div>
-                                </div>
-                                <div class="col-sm-4 col-6">
-                                    <div class="service-next service-media">
-                                        <div class="content">
-                                            <h4 class="title">Pay Per Click</h4>
-                                            <n-link to="/service-details" class="link">Next <i class="icofont-rounded-double-right"></i></n-link>
-                                        </div>
-                                        <div class="img">
-                                            <img src="/images/service-details/sm-2.png" alt="images-not_found" />
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        <!-- <ServiceCarousel /> -->
+        <ServiceDetailsContent />
 
         <Footer />
 
@@ -176,6 +25,7 @@
             OffCanvasMobileMenu: () => import('@/components/OffCanvasMobileMenu'),
             Breadcrumb: () => import('@/components/Breadcrumb'),
             ServiceCarousel: () => import('@/components/ServiceCarousel'),
+            ServiceDetailsContent: () => import('@/components/ServiceDetailsContent'),
             Footer: () => import('@/components/Footer'),
             ScrollTop: () => import('@/components/ScrollTop'),
         },

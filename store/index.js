8911e144f3c8a7e75b2dcdc476b9f5fa04ba57ee
Stefan Dragicevic <stefandragicevic@Stefan-Dragicevic.local>
2021-09-01 13:29:55 +0200

    Add blog page categories and categorie listing

--- store/index.js
+++ store/index.js
@@ -6,7 +6,8 @@ Vue.use(Vuex)
 
 export const state = () => ({
     articles: [],
-    article: []
+    article: [],
+    categories: []
 })
   
 export const mutations = {
@@ -16,6 +17,10 @@ export const mutations = {
 
     SET_ARTICLE(state, ARTICLE) {
         state.article = ARTICLE
+    },
+
+    SET_CATEGORIES(state, CATEGORIES) {
+        state.categories = CATEGORIES
     }
 }
 
@@ -36,6 +41,25 @@ export const actions = {
             .then(response => {
                 const ARTICLE = response.data
                 commit('SET_ARTICLE', ARTICLE)
+            })
+            .catch(error => console.log(error))
+    },
+
+    getCategories({ commit }) {
+        axios
+            .get('http://localhost:1337/categories')
+            .then(response => {
+                const CATEGORIES = response.data
+                let categoriesList = []
+
+                CATEGORIES.forEach(category => {
+                    let categoryName = category.category
+                    let categoryCount = category.articles.length
+                    let categorySlug = category.slug
+                    categoriesList.push({categoryName, categoryCount, categorySlug})
+                });
+
+                commit('SET_CATEGORIES', categoriesList)
             })
             .catch(error => console.log(error))
     }
